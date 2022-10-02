b93a2670e6aebe08ac643cbc3854e421c284c5af
ekkonomics <omer1@hotmail.dk>
2022-10-03 00:32:42 +0200

    test

--- components/WeatherScreen.js
+++ components/WeatherScreen.js
@@ -1,17 +1,10 @@
 import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  ActivityIndicator,
-  FlatList,
-} from "react-native";
+import {StyleSheet,Text, View, ActivityIndicator, FlatList, } from "react-native";
 
 //Eksportere så vi kan importere den i App.js
 export default function WeatherScreenC(){
   // Loading const/UseState kan fortælle os om data'en er ved at loade eller den er fejlet.
-    const [isLoading, setLoading] = useState(true);
+    const [Load, setLoading] = useState(true);
     const [data, setData] = useState([]);
   
     // Ved hjælp af en async funktion henter vi data fra openweathermap API'en, som henter data fra London vha vores API Key.
@@ -20,8 +13,8 @@ export default function WeatherScreenC(){
         const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=7e002da2d49214cdf4d0880471bb8053');
         const json = await response.json();
         setData(json.weather);
-      } catch (error) {
-        console.error(error);
+      } catch (err) {
+        console.error(err);
       } finally {
         setLoading(false);
       }
@@ -32,8 +25,8 @@ export default function WeatherScreenC(){
     }, []);
   // Når data'en er hentet kan vi liste dem vha. kalde på id'et fra json data'en. 
     return (
-      <View style={{ flex: 1, padding: 24 }}>
-        {isLoading ? <ActivityIndicator/> : (
+      <View style={{ flex: 1, padding: 48 }}>
+        {Load ? <ActivityIndicator/> : (
           <FlatList
             data={data}
             keyExtractor={({ id }, index) => id}
@@ -54,6 +47,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#0099FF',
     alignItems: "center",
-    marginTop: 48,
   }
 });
